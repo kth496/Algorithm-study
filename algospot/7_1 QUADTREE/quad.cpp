e64ea88418a8f85d68b66cf8d8828cf66ec5d1da
kth496 <taehongkim496@gmail.com>
2020-07-09 22:56:45 +0900

    Update algospot

---
+++ algospot/7_1 QUADTREE/quad.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_SIZE 9999
+
+char decompressed[MAX_SIZE][MAX_SIZE];
+
+void decompress(string::iterator& it, int y, int x, int size) {
+    char head = *(it++);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
